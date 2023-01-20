132fd7ea2cc35d5df49f91d711093668f1c4dd4c
Wendel Fernando Alves <121757701+WendelAlves93@users.noreply.github.com>
2023-01-20 16:38:16 -0300

    Update wise_receiver.ino

--- wise_receiver/wise_receiver.ino
+++ wise_receiver/wise_receiver.ino
@@ -1,9 +1,8 @@
 #include <RH_ASK.h>
 #include <RHGenericDriver.h>
 
-// Pin definitions
-const int rf_rx_pin = 8;    // Pin for RF receiver
 
+const int rf_rx_pin = 8;    // Pin for RF receiver
 RH_ASK driver(2000, rf_rx_pin);
 
 void setup() {
@@ -15,18 +14,11 @@ void loop() {
   uint8_t buflen = sizeof(buf);
 
   if (driver.recv(buf, &buflen)) {
-    // Get the sensor 1 status
     int sensor1_status = buf[0];
-    // Get the sensor 2 status
     int sensor2_status = buf[1];
-    // Get the tank sensor status
     int tank_sensor_status = buf[2];
-    // Get the soil sensor status
     int soil_sensor_status = buf[3];
-    // Get the pump status
     int pump_status = buf[4];
-    // Get the valve status
     int valve_status = buf[5];
-    // use the received data here
   }
 }
